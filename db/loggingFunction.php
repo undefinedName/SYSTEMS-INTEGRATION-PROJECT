3b07cd7efa79f8b3cd9cb5247b6a991d8aa3701c
undefinedName <56364019+undefinedName@users.noreply.github.com>
2020-01-14 19:42:17 -0500

    Update loggingFunction.php

--- db/loggingFunction.php
+++ db/loggingFunction.php
@@ -43,4 +43,60 @@ return $response;
 
 }
 }
+
+function addErrors($id, $error)
+{
+if ($id == "fe") {
+        
+	//Added these to the top, should add new errors
+	$file = "frontLog.txt";
+        $content = file_get_contents($file);
+        $content .= $error;
+	file_put_contents($file, $content);
+	
+	$file = fopen("frontLog.txt", "r");
+        $errorList = array();
+        while(!feof($file)){
+              $line = fgets($file);
+              $errorList[] = $line;
+              }
+        fclose($file);
+$response = $errorList;
+return $response;
+}
+if ($id == "be") {
+        
+	$file = "frontLog.txt";
+        $content = file_get_contents($file);
+        $content .= $error;
+	file_put_contents($file, $content);
+
+	$file = fopen("backLog.txt", "r");
+        $errorList = array();
+        while(!feof($file)){
+              $line = fgets($file);
+              $errorList[] = $line;
+              }
+        fclose($file);
+$response = $errorList;
+return $response;
+}
+if ($id == "db") {
+
+	$file = "frontLog.txt";
+        $content = file_get_contents($file);
+        $content .= $error;
+	file_put_contents($file, $content);
+
+        $file = fopen("dbLog.txt", "r");
+        $errorList = array();
+        while(!feof($file)){
+              $line = fgets($file);
+              $errorList[] = $line;
+              }
+        fclose($file);
+$response = $errorList;
+return $response;
+}
+}
 ?>
